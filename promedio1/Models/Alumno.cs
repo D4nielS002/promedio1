1a56ecabe92b4966effa45d1116c77cdd15fd729
D4nielS002 <ds4472718@gmail.com>
2024-12-03 09:31:36 -0500

    Agregar archivos de proyecto.

---
+++ promedio1/Models/Alumno.cs
@@ -0,0 +1,19 @@
+﻿namespace promedio1.Models
+{
+    public class Alumno
+    {
+
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public double Nota1 { get; set; } //accesorios de la propiedad nota 1
+        public double Nota2 { get; set; } //accesorios de la propiedad nota 
+        public double Promedio
+        {
+            get
+            {
+                return (Nota1 + Nota2) / 2;
+            }
+
+        }
+    }
+}
